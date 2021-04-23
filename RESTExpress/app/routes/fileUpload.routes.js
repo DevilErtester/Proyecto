5554501dacdbd2ce1d22ca5261680776baca4d97
Mario Chaves <xX1mario1Xx63@gmail.com>
2021-04-23 17:21:57 +0200

    added login verification to file uploads

--- RESTExpress/app/routes/fileUpload.routes.js
+++ RESTExpress/app/routes/fileUpload.routes.js
@@ -1,6 +1,9 @@
 const router = require("express").Router();
 
 const filesUp = require("../controllers/fileUpload.controller.js");
+const Login = require("../controllers/login.controller.js");
+
+router.use('*', Login.verifyLogin);
 router.post('/uploadFile', filesUp.uploadAvatar);
 
 
