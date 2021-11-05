dfe96dac52de16048e9a40ba70c977f06277b428
Mario Chaves <xX1mario1Xx63@gmail.com>
2021-11-05 21:23:39 +0100

    interceptor axios thing

--- vue-js-client-crud/src/http-common.js
+++ vue-js-client-crud/src/http-common.js
@@ -1,10 +1,10 @@
 import axios from "axios";
-// import router from './router.js'
+import router from './router.js'
 // axios.defaults.withCredentials  =true; // set cross domain and access permission to allow cookie information to be carried across domains
 
 // axios.defaults.headers.post['Content-Type'] = 'application/json'
 // axios.defaults.headers.put['Content-Type'] = 'application/json'
-export default axios.create({
+const axiosInstance = axios.create({
   baseURL: "http://localhost:8080/api",
   headers: {
     "Content-type": "application/json"
@@ -20,22 +20,24 @@ export default axios.create({
 //     return Promise.reject(error);
 //   }
 // );
-// axios.interceptors.response.use(
-//   response => {
-//     //Not logged in or session expired
-//     if ('401' === response.data.code) {
-//       //Redirect to login page
-//       router.replace({
-//         path: '/login',
-//         query: {redirect: router.currentRoute.fullPath}
-//       })
-//     }
-//     return response;
-//   },
-//   error => {
-//     if (500 === error.response.status) {
-//       //Server exception  
-//     }
-//     return  Promise.reject (error) // returns the error information returned by the interface
-//   }
-// );
+axiosInstance.interceptors.response.use(
+  response => {
+    //Not logged in or session expired
+    if (401 === response.status) {
+      console.log("a")
+      //Redirect to login page
+      router.replace({
+        path: '/login',
+        query: { redirect: router.currentRoute.fullPath }
+      })
+    }
+    return response;
+  },
+  error => {
+    if (500 === error.response.status) {
+      //Server exception  
+    }
+    return Promise.reject(error) // returns the error information returned by the interface
+  }
+);
+export default axiosInstance;
